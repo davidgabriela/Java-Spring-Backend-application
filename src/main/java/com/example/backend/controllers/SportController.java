a87c9f327cb074aa15b1d1ff996a666a359fa4c8
Cornelia-Gabriela DAVID (101607) <cornelia.david@stud.acs.upb.ro>
2021-04-15 15:16:08 +0300

    Updated UpdateSportName to UpdateSportData

--- src/main/java/com/example/backend/controllers/SportController.java
+++ src/main/java/com/example/backend/controllers/SportController.java
@@ -33,9 +33,16 @@ public class SportController {
     }
 
     @PutMapping("/sports/{id}")
-    public Sport updateSportName(@PathVariable Integer id, @RequestBody Sport sport) {
+    public Sport updateSportData(@PathVariable Integer id, @RequestBody Sport sport) {
         Sport newSport = sportRepository.getSportById(id);
-        newSport.setName(sport.getName());
+        if(sport.getName() != null)
+            newSport.setName(sport.getName());
+        if(sport.getStartDate() != null)
+            newSport.setStartDate(sport.getStartDate());
+        if(sport.getEndDate() != null)
+            newSport.setEndDate(sport.getEndDate());
+        if(sport.getPricePerDay() != null)
+            newSport.setPricePerDay(sport.getPricePerDay());
         return sportRepository.save(newSport);
     }
 }
